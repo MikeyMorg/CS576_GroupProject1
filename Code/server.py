c875acfffd86e3d202758a1d21e005ec0e321770
Michael Morgan <109762769+MikeyMorg@users.noreply.github.com>
2026-02-24 19:51:04 -0800

    Added Main and Client Handling
    Also added text instructions for testing

--- Code/server.py
+++ Code/server.py
@@ -20,7 +20,7 @@ def encode_message(msg: str) -> str:
     # Force incoming message to be limited to the maximum size
     if len(msg) > MAX_SIZE:
         print("Message exceeds max length of 256 characters.")
-        return 0
+        return None
     
     # Create an array for the encoded message
     encoded_msg = []
@@ -32,6 +32,7 @@ def encode_message(msg: str) -> str:
         # Ensure only printable ASCII messages are in the inputted message
         if encode < 32 or encode > 126:
             print("Message contains invalid ASCII characters that cannot be printed.")
+            return None
         
         # Wrap if we're at the last printable ASCII character '~', to ' '. (Space)
         if encode == 126: 
@@ -41,3 +42,63 @@ def encode_message(msg: str) -> str:
 
     return ''.join(encoded_msg)
 
+def client_handle(conn, addr):
+    #Say where we got our connection from
+    print("Connection from " + addr[0] + ":" + str(addr[1]))
+
+    #Set connection timeout length
+    conn.settimeout(10)
+
+    # Create a buffer
+    buffer = b""
+
+    #While loop to read in buffer until new line is entered
+    while b"\n" not in buffer:
+        chunk = conn.recv(MAX_SIZE)
+        
+    # If recv returns empty bytes, the client disconnected
+        if not chunk:
+            print("Client disconnected.")
+            conn.close()
+            return None
+
+        # Add the chunk to the buffer
+        buffer = buffer + chunk
+
+        # Reject if max_size is exceeded. +1 for the newline character.
+        if len(buffer) > MAX_SIZE + 1:
+            print("Message is too large")
+            conn.close()
+            return None
+
+    # Decode the new line and strip the newline
+    message = buffer.decode('utf-8').rstrip("\r\n")
+    
+    #Print inputted message
+    print("Received message: " + message)
+
+    #encode the message
+    encoded_message = encode_message(message)
+
+    # Ensure message encoding was successful
+    if encoded_message is None:
+        conn.sendall("Message encoding failed.\n".encode('utf-8'))
+        conn.close()
+        return None
+    else:
+        conn.sendall((encoded_message + "\n").encode('utf-8'))
+
+    conn.close()
+
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(('', PORT))
+    server_socket.listen(10)
+    print("Server listening for a connection at current port " + str(PORT))
+
+    while True:
+        conn, addr = server_socket.accept()
+        client_handle(conn,addr)
+
+main()
